3ee22255c0c87a177c71cd8705307597fff11e23
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-12-11 20:37:53 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserRegComponentComponent } from './user-reg-component/user-reg-component.component';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	standalone: false,
-	styleUrl: './app.component.scss',
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'myFlix-Angular-client';
-
-	constructor(public dialog: MatDialog) {}
-	openUserRegistrationDialog(): void {
-		this.dialog.open(UserRegComponentComponent, {
-			width: '280px',
-		});
-	}
+    title = 'myFlix-Angular-client';
 }
