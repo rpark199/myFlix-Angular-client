c96d408c8f29782c61980eb462a1502c1375b29b
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-12-16 20:18:39 -0500

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -12,160 +12,195 @@ const apiUrl = 'https://moviflex-a914bff79426.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
-  constructor(private http: HttpClient) {}
-  // Making the api call for the user registration endpoint
-  public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
-    return this.http
-      .post(apiUrl + 'users', userDetails)
-      .pipe(catchError(this.handleError));
+  constructor(private http: HttpClient) {
   }
 
-  private getStoredToken(): any {
-    return localStorage.getItem('token');
-  }
-
-  private getStoredUser(): any {
-    return localStorage.getItem('user');
-  }
-
-  // User login endpoint
-  public userLogin(userDetails: any): Observable<any> {
-    return this.http
-      .post(apiUrl + 'login', userDetails)
-      .pipe(catchError(this.handleError));
-  }
-
-  // Get all movies endpoint
-  public getAllMovies(): Observable<any> {
-    const token = this.getStoredToken();
-    return this.http
-      .get(apiUrl + 'movies', {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Get one movie
-  public getMovie(title: string): Observable<any> {
-    const token = this.getStoredToken();
-    return this.http
-      .get(apiUrl + 'movies/' + title, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Get director
-  public getDirector(name: string) {
-    const token = this.getStoredToken();
-    return this.http
-      .get(apiUrl + 'movies/director/' + name, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Get genre
-  public getGenre(name: string) {
-    const token = this.getStoredToken();
-    return this.http
-      .get(apiUrl + 'movies/genre/' + name, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Get all users
-  public getAllUses() {
-    const token = this.getStoredToken();
-    return this.http
-      .get(apiUrl + 'users', {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Add a movie to favourite Movies
-  public addUserFavoriteMovie(movieId: string) {
-    const token = this.getStoredToken();
-    let user = this.getStoredUser();
-    return this.http
-      .put(apiUrl + 'users/' + user.username + '/favorite/' + movieId, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Delete a movie from favourite Movies
-  public deleteUserFavoriteMovie(movieId: string) {
-    const token = this.getStoredToken();
-    let user = this.getStoredUser();
-    return this.http
-      .delete(apiUrl + 'users/' + user.username + '/favorite/' + movieId, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Edit user
-  public editUser(userDetails: any): Observable<any> {
-    const token = this.getStoredToken();
-    let user = this.getStoredUser();
-    return this.http
-      .put(apiUrl + 'users/' + user.username, userDetails, {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
-
-  // Delete user
-  public deleteUser(userDetails: any): Observable<any> {
-    const token = this.getStoredToken();
-    let user = this.getStoredUser();
-    return this.http
-      .delete(apiUrl + 'users/' + user.username, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  private getToken(): string {
+      const user = localStorage.getItem('user');
+      return user ? JSON.parse(user).token : '';
   }
 
   private handleError(error: HttpErrorResponse): any {
-    if (error.error instanceof ErrorEvent) {
-      console.error('Some error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
-      );
-    }
-    return throwError('Something bad happened; please try again later.');
+      if (error.error instanceof ErrorEvent) {
+          console.error('Some error occurred:', error.error.message);
+      } else {
+          console.error(
+              `Error Status code ${error.status}, ` +
+              `Error body is: ${error.error}`
+          );
+      }
+      return throwError('Something bad happened; please try again later.');
   }
 
+  /**
+   * create new user
+   * @param {Object} userDetails must include username, password and password. Optional: birthday
+   * @returns 
+   */
+  public userRegistration(userDetails: any): Observable<any> {
+      return this.http.post(apiUrl + '/user', userDetails)
+          .pipe(catchError(this.handleError));
+  }
+
+  /**
+   * login
+   * @param {Object} userDetails must include username and password
+   * @returns 
+   */
+  public userLogin(userDetails: any): Observable<any> {
+      return this.http.post(apiUrl + `/login?username=${userDetails.username}&password=${userDetails.password}`, userDetails)
+          .pipe(map(this.extractResponseData), catchError(this.handleError))
+  }
+
+  /**
+   * get all movies
+   * @returns if token is false, status 401 & text "unauthorized", else return array of movie object
+   */
+  public getAllMovies(): Observable<any> {
+      return this.http.get(apiUrl + '/movies', {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * get movie with specific id
+   * @param {string} id 
+   * @returns if token is false, status 401 & text "unauthorized". if movie exists, status 200 & movie object. if movie doesn't exist, status 400 & text "No such movie"
+   */
+  public getMovieWithID(id: string): Observable<any> {
+      let t = JSON.parse(localStorage.getItem("user") || "").token
+      return this.http.get(apiUrl + `/movieid/${id}`, {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * get movie with title
+   * @param {string} title Movie's title
+   * @returns if token is false, status 401 & text "unauthorized". if movie exists, status 200 & movie object. if movie doesn't exist, status 400 & text "No such movie"
+   */
+  public getMovieWithTitle(title: string): Observable<any> {
+      return this.http.get(apiUrl + `/movie/${title}`, {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * get director with name
+   * @param {string} directorName 
+   * @returns if token is false, status 401 & text "unauthorized". if director exists, status 200 & director object. if director doesn't exist, status 400 & text "No such director"
+   */
+  public getDirector(directorName: string): Observable<any> {
+      return this.http.get(apiUrl + `/director/${directorName}`, {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * get all user
+   * @returns if token is false, status 401 & text "unauthorized", else return array of user objects
+   */
+  public getUserList(): Observable<any> {
+      return this.http.get(apiUrl + `/users`, {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * get user with id
+   * @param {string} id 
+   * @returns if token is false, status 401 & text "unauthorized". if user exists, status 200 & user object. if user doesn't exist, status 400
+   */
+  public getUserByID(id: string): Observable<any> {
+      return this.http.get(apiUrl + `/user/${id}`, {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * add movie to user's favorite list
+   * @param userID 
+   * @param title movie's title
+   * @returns if token is false, status 401 & text "unauthorized". if add movie success, status 200 & user object. if movie doesn't exist, status 400 & text "No such movie"
+   */
+  public addFavoriteMovie(userID: string, title: string): Observable<any> {
+      return this.http.post(apiUrl + `/user/${userID}/${title}`, {}, {headers: new HttpHeaders(
+      {
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * delete a movie from user's favorite list
+   * @param userID 
+   * @param title 
+   * @returns if token is false, status 401 & text "unauthorized". if delete movie success, status 200 & user object.
+   */
+  public deleteFavoriteMovie(userID: string, title: string): Observable<any> {
+      return this.http.delete(apiUrl + `/user/${userID}/${title}`,
+      {headers: new HttpHeaders({
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * change user's details
+   * @param userDetails must include all the fields of user object
+   * @returns if token is false, status 401 & text "unauthorized". if update user's details success, status 200 & user object.
+   */
+  public editUser(userDetails: any): Observable<any> {
+      return this.http.put(apiUrl + `/user/${userDetails.id}`, userDetails,
+      {headers: new HttpHeaders({
+          Authorization: `Bearer ${this.getToken()}`,
+      })}).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+
+  /**
+   * delete user
+   * @param userID 
+   * @returns if token is false, status 401 & text "unauthorized". if delete user success, status 200 & text "${username} was deleted"
+   */
+  public deleteUser(userID: string): Observable<any> {
+      const body = JSON.stringify({"id": userID});
+      return this.http.delete(apiUrl + `/user`,
+      {headers: new HttpHeaders({
+          Authorization: `Bearer ${this.getToken()}`,
+      }), body: body
+      }).pipe(
+          map(this.extractResponseData), catchError(this.handleError)
+      );
+  }
+  
   // Non-typed response extraction
   private extractResponseData(res: any): any {
-    const body = res;
-    return body || {};
+      const body = res;
+      return body || { };
   }
 }
