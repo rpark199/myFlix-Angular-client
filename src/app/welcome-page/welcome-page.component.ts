023f9b13c97c3058936064d728222e20c78c21a0
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-12-27 16:25:49 -0500

    Add JSDoc comments

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import { UserLoginFormComponent } from "../user-login-form/user-login-form.component";
-import { UserRegistrationFormComponent } from "../user-registration-form/user-registration-form.component";
-import { MatDialog } from "@angular/material/dialog";
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 
+/**
+ * Component representing the welcome page of the application.
+ * It provides methods to open dialogs for user registration and login.
+ * The user registration dialog is opened when the signup button is clicked.
+ * The user login dialog is opened when the login button is clicked.
+ */
 @Component({
-  selector: "app-welcome-page",
-  templateUrl: "./welcome-page.component.html",
-  styleUrls: ["./welcome-page.component.scss"],
+  selector: 'app-welcome-page',
   standalone: false,
+
+  templateUrl: './welcome-page.component.html',
+  styleUrl: './welcome-page.component.scss',
 })
-export class WelcomePageComponent implements OnInit {
+export class WelcomePageComponent {
   constructor(public dialog: MatDialog) {}
+
   ngOnInit(): void {}
 
   /**
-   * Function to open user registration dialog
+   * Opens the user registration dialog.
+   * This function is triggered when the signup button is clicked.
+   * The dialog is opened with a width of 280px.
    */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-      width: "280px",
+      width: '280px',
     });
   }
 
   /**
-   * Function to open user login dialog
+   * Opens the user login dialog.
+   * This function is triggered when the login button is clicked.
+   * The dialog is opened with a width of 280px.
    */
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
-      width: "280px",
+      width: '280px',
     });
   }
 }
