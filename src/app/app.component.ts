c5c57036e325d32f7bde4da9741936f29c130e87
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-12-27 15:46:41 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,22 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.scss'],
+  standalone: false, // ensure that the component is not treated as a standalone component
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public router: Router) {}
 
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
+  logout(): void {
+    // clear local storage
+    localStorage.clear();
 
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
+    //redirect to welcome page
+    this.router.navigate(['welcome']);
   }
 }
